f54015fb98a393ce1419e44e481aea83f1a02ba2
Yeehok <yeehok@gmail.com>
2019-01-11 16:23:59 +0800

    Update two_sum.cpp

--- Codes/two_sum.cpp
+++ Codes/two_sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
-public
-	vectorint twoSum(vectorint& nums, int target) {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
 
-		int len = static_castunsigned int (nums.size());
+		int len = static_cast<unsigned int> (nums.size());
 
-		stdsetint nums_set;
-		stdvectorint ans_indices;
-		stdmapint, int num_index;
+		std::set<int> nums_set;
+		std::vector<int> ans_indices;
+		std::map<int, int> num_index;
 
-		for (int i = len - 1; i = 0; --i)
+		for (int i = len - 1; i >= 0; --i)
 		{
 			nums_set.insert(nums[i]);
 			num_index[nums[i]] = i;
 		}
 
-		for (int i = 0; i  len; ++i)
+		for (int i = 0; i < len; ++i)
 		{
 			int oi = num_index[target - nums[i]];
 			if (oi != i
